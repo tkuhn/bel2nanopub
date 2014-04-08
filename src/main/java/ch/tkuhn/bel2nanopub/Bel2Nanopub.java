367c6cb23e2d0f9e96bdedfbeda4397ceebee678
Tobias Kuhn <kuhntobias@gmail.com>
2014-04-08 15:28:46 +0200

    Parse statements

--- src/main/java/ch/tkuhn/bel2nanopub/Bel2Nanopub.java
+++ src/main/java/ch/tkuhn/bel2nanopub/Bel2Nanopub.java
@@ -6,18 +6,36 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import org.openbel.bel.model.BELParseErrorException;
+import org.openbel.bel.model.BELStatement;
+import org.openbel.bel.model.BELStatementGroup;
 import org.openbel.framework.common.bel.parser.BELParseResults;
 import org.openbel.framework.common.bel.parser.BELParser;
+import org.openbel.framework.common.model.Statement;
 
 public class Bel2Nanopub {
 
 	public static void main(String[] args) {
 		String belDoc = getResourceAsString("/examples/pubmed9202001.bel");
+		System.out.println("INPUT FILE:");
+		System.out.println("---");
 		System.out.println(belDoc);
 		System.out.println("---");
 		BELParseResults result = BELParser.parse(belDoc);
-		for (BELParseErrorException ex : result.getSyntaxErrors()) {
-			System.err.println(ex.getMessage());
+
+		if (!result.getSyntaxErrors().isEmpty()) {
+			System.err.println("ERRORS:");
+			for (BELParseErrorException ex : result.getSyntaxErrors()) {
+				System.err.println(ex.getMessage());
+			}
+		}
+
+		for (BELStatementGroup g : result.getDocument().getBelStatementGroups()) {
+			for (BELStatement bst : g.getStatements()) {
+				String s = bst.getStatementSyntax();
+				System.out.println("STATEMENT: " + s);
+				Statement st = BELParser.parseStatement(s);
+				if (st != null) System.out.println("PARSED SUCCESSFULLY.");
+			}
 		}
 	}
 
