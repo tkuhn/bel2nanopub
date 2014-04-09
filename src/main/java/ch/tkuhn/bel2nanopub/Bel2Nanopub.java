ee56734822df279c50c2d7a6b2d8dd56ee1742e6
Tobias Kuhn <kuhntobias@gmail.com>
2014-04-09 16:56:06 +0200

    Map BEL functions; read from file instead of resource

--- src/main/java/ch/tkuhn/bel2nanopub/Bel2Nanopub.java
+++ src/main/java/ch/tkuhn/bel2nanopub/Bel2Nanopub.java
@@ -1,9 +1,10 @@
 package ch.tkuhn.bel2nanopub;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import net.trustyuri.rdf.TransformNanopub;
 
@@ -16,15 +17,19 @@ import org.openbel.bel.model.BELStatementGroup;
 import org.openbel.framework.common.bel.parser.BELParseResults;
 import org.openbel.framework.common.bel.parser.BELParser;
 import org.openbel.framework.common.model.Statement;
+import org.openbel.framework.common.model.Term;
+import org.openrdf.model.BNode;
 import org.openrdf.model.URI;
-import org.openrdf.model.impl.URIImpl;
+import org.openrdf.model.impl.BNodeImpl;
+import org.openrdf.model.impl.LiteralImpl;
 import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.model.vocabulary.RDFS;
 import org.openrdf.rio.RDFFormat;
 
 public class Bel2Nanopub {
 
 	public static void main(String[] args) {
-		String belDoc = getResourceAsString("/examples/pubmed14734561.bel");
+		String belDoc = readFile(args[0]);
 		System.out.println("INPUT FILE:");
 		System.out.println("---");
 		System.out.println(belDoc);
@@ -38,46 +43,48 @@ public class Bel2Nanopub {
 			}
 		}
 
+		int bnodeCount = 0;
+
 		for (BELStatementGroup g : result.getDocument().getBelStatementGroups()) {
 			for (BELStatement bst : g.getStatements()) {
 				String s = bst.getStatementSyntax();
 				Statement st = BELParser.parseStatement(s);
 				if (st == null) continue;
 				System.out.println("BEL: " + s);
+				NanopubCreator npCreator = new NanopubCreator("http://www.tkuhn.ch/bel2nanopub/");
+				npCreator.addNamespace("belv", BelRdfVocabulary.BELV_NS);
+				npCreator.addNamespace("np", "http://www.nanopub.org/nschema#");
+				npCreator.addNamespace("rdfs", RDFS.NAMESPACE);
+				BNode topBn = new BNodeImpl("" + bnodeCount++);
+				npCreator.addAssertionStatement(topBn, RDFS.LABEL, new LiteralImpl(s));
 				if (st.getRelationshipType() == null) {
-					NanopubCreator npCreator = new NanopubCreator("http://www.tkuhn.ch/bel2nanopub/");
-					URI termUri = new URIImpl("http://www.tkuhn.ch/bel2nanopub/Term");
-					npCreator.addNamespace("belv", BelRdfVocabulary.BELV_NS);
-					npCreator.addNamespace("np", "http://www.nanopub.org/nschema#");
-					npCreator.addAssertionStatement(termUri, RDF.TYPE, BelRdfVocabulary.Term);
-					try {
-						Nanopub np = TransformNanopub.transform(npCreator.finalizeNanopub());
-						System.out.println("NANOPUB:");
-						NanopubUtils.writeToStream(np, System.out, RDFFormat.TRIG);
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
+					npCreator.addAssertionStatement(topBn, RDF.TYPE, BelRdfVocabulary.Term);
+					Term subj = st.getSubject();
+					URI funcUri = BelRdfVocabulary.getFunction(subj.getFunctionEnum().getAbbreviation());
+					npCreator.addAssertionStatement(topBn, RDF.TYPE, funcUri);
 				} else {
-					// TODO
+					npCreator.addAssertionStatement(topBn, RDF.TYPE, BelRdfVocabulary.Statement);
+				}
+				try {
+					Nanopub np = TransformNanopub.transform(npCreator.finalizeNanopub());
+					System.out.println("NANOPUB:");
+					NanopubUtils.writeToStream(np, System.out, RDFFormat.TRIG);
+				} catch (Exception ex) {
+					ex.printStackTrace();
 				}
 				System.out.println("---");
 			}
 		}
 	}
 
-	private static String getResourceAsString(String resourceName) {
+	private static String readFile(String path) {
 		try {
-			StringBuffer sb = new StringBuffer();
-			InputStream is = Bel2Nanopub.class.getResourceAsStream(resourceName);
-			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-			for (int c = br.read(); c != -1; c = br.read()) {
-				sb.append((char) c);
-			}
-			return sb.toString();
+			byte[] encoded = Files.readAllBytes(Paths.get(path));
+			return Charset.forName("UTF-8").decode(ByteBuffer.wrap(encoded)).toString();
 		} catch (IOException ex) {
 			ex.printStackTrace();
+			return null;
 		}
-		return null;
 	}
 
 }
--- src/main/java/ch/tkuhn/bel2nanopub/BelRdfVocabulary.java
+++ src/main/java/ch/tkuhn/bel2nanopub/BelRdfVocabulary.java
@@ -1,8 +1,16 @@
 package ch.tkuhn.bel2nanopub;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openrdf.model.URI;
 import org.openrdf.model.impl.URIImpl;
 
+/**
+ * Namespaces and mappings as defined and used by bel2rdf.
+ *
+ * @author Tobias Kuhn
+ */
 public class BelRdfVocabulary {
 
 	private BelRdfVocabulary() {}  // no instances allowed
@@ -11,5 +19,37 @@ public class BelRdfVocabulary {
 
 	public static final URI Term = new URIImpl(BELV_NS + "Term");
 	public static final URI Statement = new URIImpl(BELV_NS + "Statement");
+
+	private static Map<String,URI> functionMap = new HashMap<String,URI>();
+
+	static {
+		functionMap.put("a", new URIImpl(BELV_NS + "Abundance"));
+		functionMap.put("g", new URIImpl(BELV_NS + "GeneAbundance"));
+		functionMap.put("p", new URIImpl(BELV_NS + "ProteinAbundance"));
+		functionMap.put("r", new URIImpl(BELV_NS + "RNAAbundance"));
+		functionMap.put("m", new URIImpl(BELV_NS + "microRNAAbundance"));
+		functionMap.put("complex", new URIImpl(BELV_NS + "ComplexAbundance"));
+		functionMap.put("composite", new URIImpl(BELV_NS + "CompositeAbundance"));
+		functionMap.put("bp", new URIImpl(BELV_NS + "BiologicalProcess"));
+		functionMap.put("path", new URIImpl(BELV_NS + "Pathology"));
+		functionMap.put("rxn", new URIImpl(BELV_NS + "Reaction"));
+		functionMap.put("tloc", new URIImpl(BELV_NS + "Translocation"));
+		functionMap.put("sec", new URIImpl(BELV_NS + "CellSecretion"));
+		functionMap.put("deg", new URIImpl(BELV_NS + "Degradation"));
+		functionMap.put("cat", new URIImpl(BELV_NS + "AbundanceActivity"));
+		functionMap.put("chap", new URIImpl(BELV_NS + "AbundanceActivity"));
+		functionMap.put("gtp", new URIImpl(BELV_NS + "AbundanceActivity"));
+		functionMap.put("kin", new URIImpl(BELV_NS + "AbundanceActivity"));
+		functionMap.put("act", new URIImpl(BELV_NS + "AbundanceActivity"));
+		functionMap.put("pep", new URIImpl(BELV_NS + "AbundanceActivity"));
+		functionMap.put("phos", new URIImpl(BELV_NS + "AbundanceActivity"));
+		functionMap.put("ribo", new URIImpl(BELV_NS + "AbundanceActivity"));
+		functionMap.put("tscript", new URIImpl(BELV_NS + "AbundanceActivity"));
+		functionMap.put("tport", new URIImpl(BELV_NS + "AbundanceActivity"));
+	}
+
+	public static URI getFunction(String abbrev) {
+		return functionMap.get(abbrev);
+	}
 
 }
