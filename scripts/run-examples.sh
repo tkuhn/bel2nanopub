18b943d4360b43c91113755fed11c4eda176a3aa
Tobias Kuhn <kuhntobias@gmail.com>
2014-04-22 11:52:48 +0200

    Transform examples with new RDF conversion from bel.rb

--- scripts/run-examples.sh
+++ scripts/run-examples.sh
@@ -9,3 +9,9 @@ cd ..
 scripts/Bel2Nanopub.sh -c $ORCID src/main/resources/examples/pubmed9202001.bel > src/main/resources/examples/pubmed9202001.out.txt
 scripts/Bel2Nanopub.sh -c $ORCID src/main/resources/examples/pubmed14734561.bel > src/main/resources/examples/pubmed14734561.out.txt
 scripts/Bel2Nanopub.sh -c $ORCID src/main/resources/examples/pubmed16679305.bel > src/main/resources/examples/pubmed16679305.out.txt
+
+H='# File produced by bel.rb (https://github.com/OpenBEL/bel.rb)'
+
+( echo $H ; bel2rdf --bel src/main/resources/examples/pubmed9202001.bel -f turtle ) > src/main/resources/examples/pubmed9202001.ttl
+( echo $H ; bel2rdf --bel src/main/resources/examples/pubmed14734561.bel -f turtle ) > src/main/resources/examples/pubmed14734561.ttl
+( echo $H ; bel2rdf --bel src/main/resources/examples/pubmed16679305.bel -f turtle ) > src/main/resources/examples/pubmed16679305.ttl
