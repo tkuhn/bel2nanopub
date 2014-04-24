ef39b483bf79c5a7391e0a8b2784a6d06f42e78c
Tobias Kuhn <kuhntobias@gmail.com>
2014-04-24 16:35:22 +0200

    Download latest BEL files

--- scripts/download-files.sh
+++ scripts/download-files.sh
@@ -2,8 +2,6 @@
 
 mkdir -p downloads
 
-wget -O downloads/nsmap.ttl http://build.openbel.org/browse/OR-BR/latest/artifact/JOB1/Namespace-turtle-export/testfile.ttl
-# if latest build failed:
-#wget -O downloads/nsmap.ttl http://build.openbel.org/browse/OR-BR-84/artifact/JOB1/Namespace-turtle-export/testfile.ttl
+wget -O downloads/nsmap.ttl http://build.openbel.org/artifact/OR-BR/JOB1/build-latest/Namespace-turtle-export/testfile.ttl
 
-wget -O downloads/hgnc-human-genes.belns http://resource.belframework.org/belframework/20131211/namespace/hgnc-human-genes.belns
+wget -O downloads/hgnc-human-genes.belns http://build.openbel.org/artifact/OR-BR/JOB1/build-latest/Namespaces/hgnc-human-genes.belns
