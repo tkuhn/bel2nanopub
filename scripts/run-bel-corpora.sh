0d901af3fda380692c9b22cecc057cd6ea43987c
Tobias Kuhn <kuhntobias@gmail.com>
2014-07-02 10:15:17 +0200

    Improve script for processing the BEL small/large corpus

--- scripts/run-bel-corpora.sh
+++ scripts/run-bel-corpora.sh
@@ -3,28 +3,37 @@
 # ORCID IDs of creators:
 ORCIDS="-c 0000-0002-1267-0234 -c 0000-0001-6818-334X"
 
-NS=http://resource.belframework.org/belframework/1.0/knowledge
-
-BELDIR=~/Packages/openbel-framework-resources
+URL1=http://resource.belframework.org/belframework/1.0/knowledge
+URL2=http://resource.belframework.org/belframework/20131211/knowledge
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd ..
 
-mkdir -p temp
+mkdir -p downloads
 mkdir -p output
 
-#echo "Downloading files..."
-#wget -O temp/small_corpus.original.bel http://resource.belframework.org/belframework/20131211/knowledge/small_corpus.bel
-#wget -O temp/large_corpus.original.bel http://resource.belframework.org/belframework/20131211/knowledge/large_corpus.bel
-cp $BELDIR/knowledge/small_corpus.bel temp/small_corpus.original.bel
-cp $BELDIR/knowledge/large_corpus.bel temp/large_corpus.original.bel
+echo "Downloading files for version 1.0..."
+wget -O downloads/small_corpus-1.0-original.bel $URL1/small_corpus.bel
+wget -O downloads/large_corpus-1.0-original.bel $URL1/large_corpus.bel
+
+echo "Downloading files for version 20131211..."
+wget -O downloads/small_corpus-20131211-original.bel $URL2/small_corpus.bel
+wget -O downloads/large_corpus-20131211-original.bel $URL2/large_corpus.bel
 
 echo "Preprocessing files (remove carriage return characters)..."
-cat temp/small_corpus.original.bel | sed 's/\r//' > temp/small_corpus.bel
-cat temp/large_corpus.original.bel | sed 's/\r//' > temp/large_corpus.bel
+cat downloads/small_corpus-1.0-original.bel | sed 's/\r//' > downloads/small_corpus-1.0.bel
+cat downloads/large_corpus-1.0-original.bel | sed 's/\r//' > downloads/large_corpus-1.0.bel
+cat downloads/small_corpus-20131211-original.bel | sed 's/\r//' > downloads/small_corpus-20131211.bel
+cat downloads/large_corpus-20131211-original.bel | sed 's/\r//' > downloads/large_corpus-20131211.bel
 
-echo "Processing small corpus..."
-scripts/Bel2Nanopub.sh $ORCIDS -u https://github.com/OpenBEL/openbel-framework-resources/blob/latest/knowledge/small_corpus.bel temp/small_corpus.bel > output/small_corpus.nanopubs.trig 2> output/small_corpus.nanopubs.err
+echo "Processing small corpus of version 1.0..."
+scripts/Bel2Nanopub.sh $ORCIDS -u $URL1/small_corpus.bel downloads/small_corpus-1.0.bel > output/small_corpus-1.0.nanopubs.trig 2> output/small_corpus-1.0.nanopubs.err
 
-echo "Processing large corpus..."
-scripts/Bel2Nanopub.sh $ORCIDS -u https://github.com/OpenBEL/openbel-framework-resources/blob/latest/knowledge/large_corpus.bel temp/large_corpus.bel > output/large_corpus.nanopubs.trig 2> output/large_corpus.nanopubs.err
+echo "Processing large corpus of version 1.0..."
+scripts/Bel2Nanopub.sh $ORCIDS -u $URL1/large_corpus.bel downloads/large_corpus-1.0.bel > output/large_corpus-1.0.nanopubs.trig 2> output/large_corpus-1.0.nanopubs.err
+
+echo "Processing small corpus of version 20131211..."
+scripts/Bel2Nanopub.sh $ORCIDS -u $URL2/small_corpus.bel downloads/small_corpus-20131211.bel > output/small_corpus-20131211.nanopubs.trig 2> output/small_corpus-20131211.nanopubs.err
+
+echo "Processing large corpus of version 20131211..."
+scripts/Bel2Nanopub.sh $ORCIDS -u $URL2/large_corpus.bel downloads/large_corpus-20131211.bel > output/large_corpus-20131211.nanopubs.trig 2> output/large_corpus-20131211.nanopubs.err
