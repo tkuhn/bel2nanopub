c0b466e4ad933f598398667adc5de7d2a191a81a
Tobias Kuhn <kuhntobias@gmail.com>
2015-03-02 12:08:26 -0800

    Fix trusty URIs (follow v1 of specification)

--- scripts/get-published-nanopubs.sh
+++ scripts/get-published-nanopubs.sh
@@ -4,19 +4,19 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd ..
 
 echo "Retrieving published nanopubs for Small Corpus 1.0..."
-scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.small_corpus-1.0.nanopubs.trig RA5E1H82MvGpCC-rM690KGQNqQJg8za7VDL8ovoJamZp8
+scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.small_corpus-1.0.nanopubs.trig RAwca4sgaf81LsmLpFxCDnr8-z7PZxuFaq_JgzHR-M7Zw
 
 echo "Retrieving published nanopubs for Large Corpus 1.0..."
-scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.large_corpus-1.0.nanopubs.trig RAflqJnKc4t94zr9air2dpSztiy2laEZAyePJQY768Zfo
+scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.large_corpus-1.0.nanopubs.trig RAaXIwHHNGrYbQQqQ_4CrTfKozhgxs2Oxzgr8_k8vYxCo
 
 echo "Retrieving published nanopubs for Small/Large Corpus 1.0..."
-scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.corpora-1.0.nanopubs.trig RAYAM0V8yiCH3lSk_CAA8L0kY4xpIWqpK9sUnsZJasMcE
+scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.corpora-1.0.nanopubs.trig RAh69y-NnlTMvakrVrJUrJEW6n0WXUxlwUwEXHai5RocU
 
 echo "Retrieving published nanopubs for Small Corpus 20131211..."
-scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.small_corpus-20131211.nanopubs.trig RAXtMFMUPEXI-MCcBE9XzlsdvVBtE7GwZRzsaM0AMym0Y
+scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.small_corpus-20131211.nanopubs.trig RAsY_oMj-L_wka_Uf7HvAryVHP6fgG5bnJv9ctxuehKCw
 
 echo "Retrieving published nanopubs for Large Corpus 20131211..."
-scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.large_corpus-20131211.nanopubs.trig RAZsyF_g9FGnYoPsa19uiZU-D4tSaFlDfRv52YBbWh7Yc
+scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.large_corpus-20131211.nanopubs.trig RABWMIlHMyKifOPh3Es_c261oGiENsgYu_vUzBGQEZimY
 
 echo "Retrieving published nanopubs for Small/Large Corpus 20131211..."
-scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.corpora-20131211.nanopubs.trig RAueDCCGkuttgQlyQJeWbPODaN4Nh9hH1l9m7Fes3z3JM
+scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.corpora-20131211.nanopubs.trig RAOIcxR5izr0EPkdLRHEyP2HkRYlGU6vkmB0Qcfu0UZEw
