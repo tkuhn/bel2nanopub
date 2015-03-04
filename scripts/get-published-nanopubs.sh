0473caa68d90528fddc46af88173f559bc8fcd9a
Tobias Kuhn <kuhntobias@gmail.com>
2015-03-04 14:00:10 -0800

    Update artifact codes of indexes

--- scripts/get-published-nanopubs.sh
+++ scripts/get-published-nanopubs.sh
@@ -10,7 +10,7 @@ echo "Retrieving published nanopubs for Large Corpus 1.0..."
 scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.large_corpus-1.0.nanopubs.trig RAsmhDKpyhTORhvWOZZL_cfSVZUqLYJFl_AqF_xPN34Qw
 
 echo "Retrieving published nanopubs for Small/Large Corpus 1.0..."
-scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.corpora-1.0.nanopubs.trig RAKUB-H_7ATOS2wemqN2yXjGdOqqlYeOuAgy5WOgVQwpg
+scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.corpora-1.0.nanopubs.trig RACy0I4f_wr62Ol7BhnD5EkJU6Glf-wp0oPbDbyve7P6o
 
 echo "Retrieving published nanopubs for Small Corpus 20131211..."
 scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.small_corpus-20131211.nanopubs.trig RAtF0ivB9B8cb-u3K_zElgmRBxiDwfym1yVBRY6VAyWvE
@@ -19,4 +19,4 @@ echo "Retrieving published nanopubs for Large Corpus 20131211..."
 scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.large_corpus-20131211.nanopubs.trig RAdw0S5f06-Ed2uXNISU-wbXmqvQ9-6hxd4fqIslT38Wg
 
 echo "Retrieving published nanopubs for Small/Large Corpus 20131211..."
-scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.corpora-20131211.nanopubs.trig RAVEEQwjHbSgw0a7WIZhVkVHOnbfTGY7HNTSDGbrkPAAg
+scripts/exec-class.sh org.nanopub.extra.server.GetNanopub -c -o output/downloaded.corpora-20131211.nanopubs.trig RAR5dwELYLKGSfrOclnWhjOj-2nGZN_8BW1JjxwFZINHw
--- scripts/make-indexes-2.sh
+++ scripts/make-indexes-2.sh
@@ -11,8 +11,8 @@ scripts/exec-class.sh org.nanopub.extra.index.MakeIndex  \
   -a http://resource.belframework.org/belframework/1.0/knowledge/small_corpus.bel \
   -a https://github.com/tkuhn/bel2nanopub \
   -c 0000-0002-1267-0234 -c 0000-0001-6818-334X \
-  -s http://np.inn.ac/RA5E1H82MvGpCC-rM690KGQNqQJg8za7VDL8ovoJamZp8 \
-  -s http://np.inn.ac/RAflqJnKc4t94zr9air2dpSztiy2laEZAyePJQY768Zfo
+  -s http://np.inn.ac/RAR7OfS-AqG9_XogObQZpWq6LaBsV95jeseJtscuwpwJo \
+  -s http://np.inn.ac/RAsmhDKpyhTORhvWOZZL_cfSVZUqLYJFl_AqF_xPN34Qw
 
 echo "Making index for Small/Large Corpus 20131211..."
 scripts/exec-class.sh org.nanopub.extra.index.MakeIndex  \
@@ -22,5 +22,5 @@ scripts/exec-class.sh org.nanopub.extra.index.MakeIndex  \
   -a http://resource.belframework.org/belframework/1.0/knowledge/small_corpus.bel \
   -a https://github.com/tkuhn/bel2nanopub \
   -c 0000-0002-1267-0234 -c 0000-0001-6818-334X \
-  -s http://np.inn.ac/RAXtMFMUPEXI-MCcBE9XzlsdvVBtE7GwZRzsaM0AMym0Y \
-  -s http://np.inn.ac/RAZsyF_g9FGnYoPsa19uiZU-D4tSaFlDfRv52YBbWh7Yc
+  -s http://np.inn.ac/RAtF0ivB9B8cb-u3K_zElgmRBxiDwfym1yVBRY6VAyWvE \
+  -s http://np.inn.ac/RAdw0S5f06-Ed2uXNISU-wbXmqvQ9-6hxd4fqIslT38Wg
